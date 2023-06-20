ea3d22153ee969fd3acb94d391fa295279c4d93e
Byc0 <matyi0918@gmail.com>
2023-06-20 20:36:06 +0200

    Add login

--- limesz/src/App.tsx
+++ limesz/src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import { Registration } from './Auth/Registration';
+import { Login } from './Auth/Login';
+import { Home } from './Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+        <div>
+          <section>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/signup" element={<Registration />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </section>
+        </div>
+      </Router>
     </div>
   );
 }
