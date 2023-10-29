090e4aeb09f0b08f839c294f4c2db9628101a2b0
Biszterszky Mátyás <matyi0918@gmail.com>
2023-10-29 13:42:22 +0100

    no message

---
+++ limesz_app/limesz_app/Misc/GameLogic/CardGame/CustomPrompt.cs
@@ -0,0 +1,9 @@
+namespace margarita_app.Misc.GameLogic.Abstraction;
+
+public class CustomPrompt
+{
+    public string Id { get; set; }
+    public string UiType { get; set; }
+    public object InputArgs { get; set; }
+    public object Result { get; set; }
+}
